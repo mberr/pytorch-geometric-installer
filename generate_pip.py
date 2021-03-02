cb6f28ff5ddb16cbb2de6a2bfad6510c6d6b2b5e
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2021-03-02 11:44:13 +0100

    update

--- generate_pip.py
+++ generate_pip.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 if __name__ == '__main__':
     try:
         import torch
@@ -14,20 +12,21 @@ pip install torch
     cuda_version = torch.version.cuda
     cudnn_version = torch.backends.cudnn.version()
     gpu = torch.cuda.is_available()
-    pprint(dict(
-        torch=torch_version,
-        cuda=cuda_version,
-        cudnn=cudnn_version,
-        gpu=gpu,
-    ))
+    print(f"""\
+# GPU available: {gpu}
+# Versions
+# Torch: {torch_version}
+# CUDA : {cuda_version}
+# CudNN: {cudnn_version}
+""")
 
     TORCH = torch_version
     CUDA = cuda_version.replace(".", "") if gpu else "cpu"
 
-    print(f"""
-pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-{TORCH}+${CUDA}.html
-pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-{TORCH}+${CUDA}.html
-pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-{TORCH}+${CUDA}.html
-pip install --no-index torch-spline-conv -f https://pytorch-geometric.com/whl/torch-{TORCH}+${CUDA}.html
+    print(f"""\
+pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
+pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
+pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
+pip install --no-index torch-spline-conv -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html
 pip install torch-geometric
-    """)
+""")
